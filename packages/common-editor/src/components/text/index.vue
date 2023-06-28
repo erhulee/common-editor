064c3a97e5028709778e69ec52252c003b650b76
erhu <1574605559@qq.com>
2023-06-28 09:56:17 +0800

    fix: 文字效果

--- packages/common-editor/src/components/text/index.vue
+++ packages/common-editor/src/components/text/index.vue
@@ -3,22 +3,20 @@
         <input ref="inputRef" :value="props.options.material.content" :style="style" @blur="handleBlur"
             @input="handleChange" class=" w-full p-0 bg-transparent focus-visible:outline-none" />
     </foreignObject>
-    <text v-else @dblclick="handleClick" class=" select-none"  draggable="false" v-bind="svgAttribute" :fill="style.color"
-        :font-size="style['font-size']" :font-weight="style['font-weight']">
+    <text v-else ref="textRef" class="select-none" draggable="false" @click.capture="handleDBClick" v-bind="svgAttribute">
         {{ props.options.material.content }}
     </text>
 </template>
 
 <script setup lang="ts">
 import { BaseSetting, FontSetting } from '@/type/setting';
-import { mapKeys } from 'lodash-es';
+import { mapKeys, pick } from 'lodash-es';
 import { computed, nextTick, ref } from 'vue';
 function transformKeys(value: Record<string, any>) {
     return mapKeys(value, (_, key) => key.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase())
 }
 
 const emit = defineEmits(["change"]);
-
 const props = defineProps<{
     id: string,
     options: Record<string, any> & {
@@ -31,12 +29,15 @@ const props = defineProps<{
 }>();
 const isEdit = ref(false)
 const inputRef = ref<HTMLElement | null>(null)
+const textRef = ref(null)
+
 
 const style = computed(() => {
     const entry = Object.entries(props.options.font);
     return transformKeys(entry.reduce<any>((pre, [key, value]) => {
+        console.log(key, value)
         if (typeof value == "number") {
-            pre[key] = value + "px"
+            pre[key] = value
         } else {
             pre[key] = value
         }
@@ -46,6 +47,7 @@ const style = computed(() => {
         opacity: props.options.base.opacity
     }))
 });
+console.log(style)
 
 const svgAttribute = computed(() => {
     // y 坐标需要兼容 字体的特殊性
@@ -53,9 +55,11 @@ const svgAttribute = computed(() => {
         opacity: props.options.base.opacity,
         x: props.options.base.left,
         y: props.options.base.top + props.options.font.fontSize,
-
+        fill: style.value.color,
+        ...pick(style.value, 'font-size', 'font-weight', 'font-style')
     }
 })
+console.log(svgAttribute)
 
 const inputAttribute = computed(() => {
     // y 坐标需要兼容 字体的特殊性
@@ -69,7 +73,8 @@ const inputAttribute = computed(() => {
 
 
 
-function handleClick() {
+function handleDBClick() {
+    console.log("hello")
     isEdit.value = true
     nextTick(() => inputRef.value?.focus())
 }
@@ -83,6 +88,9 @@ function handleChange(e: any) {
     })
 }
 
+defineExpose({
+    handleDBClick
+})
 
 
 
